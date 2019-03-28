3cf043e3a8716d5bc26a10201ec557b4c73aaab3
Chawan42 <48669418+Chawan42@users.noreply.github.com>
2019-03-28 13:57:54 +0700

    far update:

---
+++ database/migrations/2019_03_26_085721_create_company_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companys', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('name');
+            $table->string('category');
+            $table->string('address');
+            $table->string('tel');
+            $table->string('website');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company');
+    }
+}
